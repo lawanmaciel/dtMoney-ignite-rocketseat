cddfd036018e628d6f8c8ec6a4e9d2b8065c7c02
lawanmaciel <irolawan33@gmail.com>
2021-05-01 05:51:58 -0300

    Subindo

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,63 @@
+import { useContext } from "react";
+import incomeImg from "../../assets/income.svg";
+import outcomeImg from "../../assets/outcome.svg";
+import totalImg from "../../assets/total.svg";
+import { TransactionsContext } from "../../hooks/useTransactions";
+
+import { Container } from "./styles";
+
+export function Summary() {
+
+    const { transactions } = useContext(TransactionsContext);
+
+    const summary = transactions.reduce((acc, transaction) => {
+        if (transaction.type === 'deposit') {
+            acc.deposits += transaction.amount;
+            acc.total += transaction.amount;
+        } else {
+            acc.withdaws += transaction.amount;
+            acc.total -= transaction.amount;
+        }
+        return acc;
+    }, {
+        deposits: 0,
+        withdaws: 0,
+        total: 0,
+    });
+
+    return (
+        <Container>
+            <div>
+                <header>
+                    <p>Entradas</p>
+                    <img src={incomeImg} alt="Entradas" />
+                </header>
+                <strong>
+                    {new Intl.NumberFormat('pt-BR', {
+                        style: 'currency',
+                        currency: 'BRL'
+                    }).format(summary.deposits)}
+                </strong>
+            </div>
+            <div>
+                <header>
+                    <p>Saídas</p>
+                    <img src={outcomeImg} alt="Entradas" />
+                </header>
+                <strong>
+                    - {new Intl.NumberFormat('pt-BR', {
+                    style: 'currency',
+                    currency: 'BRL'
+                }).format(summary.withdaws)}
+                </strong>
+            </div>
+            <div className="highlight-background">
+                <header>
+                    <p>Total</p>
+                    <img src={totalImg} alt="Entradas" />
+                </header>
+                <strong>{summary.total}</strong>
+            </div>
+        </Container>
+    );
+}
